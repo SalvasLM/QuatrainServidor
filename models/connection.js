3528b0556f3f5dfd7549c237125e1fb465c764a5
Salvador Marchante <salvasmaster@gmail.com>
2022-04-27 12:52:56 +0100

    c

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-const connectionString = "postgres://iyeqnhvvsnprgg:f86a67e90666581f9bd1646c04dd1db2670ba109d7d353e11369a655eff3c457@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dchcla98v59dg0\n"
+const connectionString = "postgres://awjgjzpajpsdib:b5c761f53e3d7e5583e3632f22b410550fc09099871af5d567808383e9a71c4d@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d7jubh4ukb3goe\n"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
