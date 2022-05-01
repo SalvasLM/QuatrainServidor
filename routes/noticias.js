3d3d8a99db551575d5a1d65fdeb9ff638759aabc
Salvador Marchante <salvasmaster@gmail.com>
2022-05-01 01:06:14 +0100

    c

--- routes/noticias.js
+++ routes/noticias.js
@@ -1,4 +1,4 @@
-const client = require('../models/connection.js')
+const client = require('C:/unimanager/models/connection')
 const express = require('express');
 const {log} = require("debug");
 const app = express();
