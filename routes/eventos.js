3cc196c4b608b342e9404955677740b21b40c5c2
Salvador Marchante <salvasmaster@gmail.com>
2022-05-10 22:34:18 +0100

    nhoco nhoco

--- routes/eventos.js
+++ routes/eventos.js
@@ -8,7 +8,7 @@ client.connect();
 
 
 const getEventos = (request, response) => {
-    client.query('select * from eventos', (error, results) => {
+    client.query('select eventos.*, st_X(evento_local) lat, st_Y(evento_local) long from eventos', (error, results) => {
         if (error) {
             throw error
         }
