7dca9e948137205717d2e06f7d43f7170b646fb6
Salvador Marchante <salvasmaster@gmail.com>
2022-05-26 02:47:24 +0100

    nhoco nhoco

--- routes/noticias.js
+++ routes/noticias.js
@@ -8,7 +8,7 @@ client.connect();
 
 
 const getNoticias = (request, response) => {
-    client.query('select * from noticias ORDER BY noticia_id DESC', (error, results) => {
+    client.query('select * from noticias ORDER BY noticia_id ASC', (error, results) => {
         if (error) {
             throw error
         }
